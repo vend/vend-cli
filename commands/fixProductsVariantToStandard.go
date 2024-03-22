2d42dc2ffc99d2e704ac5015c4206eb8c84ae636
edward-hayes <edward.hayes@lightspeedhq.com>
2024-03-22 18:06:21 -0400

    add progress bar to fixproductsvarianttostandard

--- commands/fixProductsVariantToStandard.go
+++ commands/fixProductsVariantToStandard.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/vend/govend/vend"
 	"github.com/vend/vend-cli/pkg/csvparser"
+	"github.com/vend/vend-cli/pkg/messenger"
+	pbar "github.com/vend/vend-cli/pkg/progressbar"
 )
 
 var fixProductsVariantToStandardCmd = &cobra.Command{
@@ -43,6 +46,15 @@ type ConvertVariantToStandardRequest struct {
 	VariantID string `json:"variant_id"`
 }
 
+type FailedConvertVariantToStandardRequest struct {
+	VariantID string
+	Reason    string
+}
+
+var failedRequests []FailedConvertVariantToStandardRequest
+
+//var url = fmt.Sprintf("https://%s.vendhq.com/api/2.0/products/actions/bulk", DomainPrefix)
+
 func fixProductsVariantToStandard() {
 	// Create new Vend Client.
 	vc := vend.NewClient(Token, DomainPrefix, "")
@@ -56,14 +68,24 @@ func fixProductsVariantToStandard() {
 	fmt.Println("\nReading CSV...")
 	ids, err := csvparser.ReadIdCSV(FilePath)
 	if err != nil {
-		log.Fatalf(color.RedString("Failed to get ids from the file: %s\nError:%s", FilePath, err))
+		err = fmt.Errorf("Failed to get IDs from the file: %s\nError:%s", FilePath, err)
+		messenger.ExitWithError(err)
+	}
+
+	// Make the requests
+	fmt.Println("\nConverting variants to standard products...")
+	p := pbar.CreateSingleBar()
+	bar, err := p.AddProgressBar(len(ids), "Converting")
+	if err != nil {
+		fmt.Println("Error creating progress bar:", err)
 	}
 
 	url := fmt.Sprintf("https://%s.vendhq.com/api/2.0/products/actions/bulk", DomainPrefix)
-	// Make the requests
+
 	maxProductsPerRequest := 10
 	reqBody := make([]ConvertVariantToStandardRequest, 0, len(ids))
 	for i, id := range ids {
+		bar.Increment()
 		// NOTE: This action is internal and could potentially change in a non-backward compatible manner.
 		body := ConvertVariantToStandardRequest{
 			Action:    "product.classification.convert_to_standard",
@@ -73,22 +95,49 @@ func fixProductsVariantToStandard() {
 		reqBody = append(reqBody, body)
 
 		if i%maxProductsPerRequest == 0 {
-			fmt.Printf("\nConverting %d variants to a standard product. \n ids: %v", len(reqBody), ids)
 			_, err = vendClient.MakeRequest(http.MethodPost, url, reqBody)
 			if err != nil {
-				fmt.Printf(color.RedString("Failed to fix product: %v", err))
-				return
+				retryConvertVariantToStandardRequests(reqBody)
 			}
 			reqBody = make([]ConvertVariantToStandardRequest, 0, maxProductsPerRequest)
 		}
 	}
+	p.Wait()
 
 	if len(reqBody) > 0 {
 		_, err = vendClient.MakeRequest(http.MethodPost, url, reqBody)
 		if err != nil {
-			fmt.Printf(color.RedString("Failed to fix product: %v", err))
+			retryConvertVariantToStandardRequests(reqBody)
+		}
+	}
+
+	if len(failedRequests) > 0 {
+		fmt.Println(color.RedString("\n\nThere were some errors. Writing failures to csv.."))
+		fileName := fmt.Sprintf("%s_failed_convert_variant_to_standard_requests_%v.csv", DomainPrefix, time.Now().Unix())
+		err := csvparser.WriteErrorCSV(fileName, failedRequests)
+		if err != nil {
+			messenger.ExitWithError(err)
 		}
 	}
 
 	fmt.Println(color.GreenString("\n\nFinished! 🎉\n"))
+}
+
+// if the group fails a request, seperate the individual requests and retry
+// log the failed requests
+func retryConvertVariantToStandardRequests(failedGroup []ConvertVariantToStandardRequest) {
+
+	url := fmt.Sprintf("https://%s.vendhq.com/api/2.0/products/actions/bulk", DomainPrefix)
+
+	for _, body := range failedGroup {
+		_, err := vendClient.MakeRequest(http.MethodPost, url, body)
+		if err != nil {
+			failedRequests = append(
+				failedRequests,
+				FailedConvertVariantToStandardRequest{
+					VariantID: body.VariantID, Reason: err.Error(),
+				})
+			continue
+		}
+	}
 }
