1ea0ee96e98e57d4a9c9ae7ff6de20a355931562
edward-hayes <edward.hayes@lightspeedhq.com>
2024-03-22 14:23:19 -0400

    add progressbar to delete-images

--- commands/deleteImages.go
+++ commands/deleteImages.go
@@ -3,14 +3,21 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/vend/vend-cli/pkg/csvparser"
 	"github.com/vend/vend-cli/pkg/messenger"
+	pbar "github.com/vend/vend-cli/pkg/progressbar"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/vend/govend/vend"
 )
+
+type FailedImageDeleteRequest struct {
+	ImageID string
+	Reason  string
+}
 
 // deleteImagesCmd represents the deleteImages command
 var deleteImagesCmd = &cobra.Command{
@@ -48,19 +55,40 @@ func deleteImages() {
 		messenger.ExitWithError(err)
 	}
 
+	failedRequests := []FailedImageDeleteRequest{}
+
 	// Make the requests
+	fmt.Println("\nDeleting images...")
+	p := pbar.CreateSingleBar()
+	bar, err := p.AddProgressBar(len(ids), "Deleting")
+	if err != nil {
+		fmt.Println("Error creating progress bar:", err)
+	}
+
 	count := 0
 	for _, id := range ids {
-		fmt.Printf("\nDeleting %v", id)
+		bar.Increment()
 		url := fmt.Sprintf("https://%s.vendhq.com/api/2.0/product_images/%s", DomainPrefix, id)
 		_, err = vendClient.MakeRequest("DELETE", url, nil)
 		if err != nil {
-			fmt.Printf(color.RedString("Failed to delete image: %v", err))
+			failedRequests = append(failedRequests, FailedImageDeleteRequest{ImageID: id, Reason: err.Error()})
 			continue
 		}
 		count += 1
 		fmt.Println(count)
 
 	}
+	p.Wait()
+
+	if len(failedRequests) > 0 {
+		fmt.Println(color.RedString("\n\nThere were some errors. Writing failures to csv.."))
+		fileName := fmt.Sprintf("%s_failed_delete_image_requests__%v.csv", DomainPrefix, time.Now().Unix())
+		err := csvparser.WriteErrorCSV(fileName, failedRequests)
+		if err != nil {
+			messenger.ExitWithError(err)
+			return
+		}
+	}
+
 	fmt.Printf(color.GreenString("\n\nFinished! 🎉\nDeleted %d out of %d images"), count, len(ids))
 }
