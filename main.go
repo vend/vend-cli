cb823aa595ccd5e2e45cbe30ca4437274a5118a9
edward-hayes <edward.hayes@lightspeedhq.com>
2024-03-26 14:07:19 -0400

    update messaging

--- main.go
+++ main.go
@@ -9,13 +9,25 @@ import (
 	"github.com/vend/vend-cli/pkg/messenger"
 )
 
+const uhOhLogo = `
+
+
+  ____  __ __   _  ______     __  __
+ / __ \/ // /  / |/ / __ \   / / / /
+/ /_/ / _  /  /    / /_/ /  /_/ /_/ 
+\____/_//_/  /_/|_/\____/  (_) (_)  
+								   `
+
 func SupressStackTrace() {
 	if r := recover(); r != nil {
+		fmt.Println(color.RedString(uhOhLogo))
 		if exit, ok := r.(messenger.Exit); ok {
-			fmt.Println(color.RedString("\n\nvendcli exited because an error occured:"))
-			fmt.Println(color.YellowString(fmt.Sprintf("%s", exit.Message)))
+			fmt.Println(color.RedString("vendcli exited because an error occured"))
+			fmt.Println("ERROR:  ", color.YellowString(fmt.Sprintf("%s", exit.Message)))
 			os.Exit(exit.Code)
 		}
+		fmt.Println(color.RedString("vendcli exited unexpectedly"))
+		fmt.Println(color.YellowString("please share the following stack trace in swarm"))
 		panic(r) // not an Exit, bubble up
 	}
 }
